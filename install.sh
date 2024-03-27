2b348c74a40b71338a2314c96106005c54b977e2
Deep Panara <39181901+deeppanara@users.noreply.github.com>
2024-03-27 13:31:39 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,3 @@ echo '##### line added by "https://github.com/deeppanara/prompt_command.git" ###
 echo 'source ~/.bash/prompt_command' >> ~/.bashrc  
 echo 'export PROMPT_COMMAND="prompt_command"' >> ~/.bashrc 
 echo '##### END #####' >> ~/.bashrc 
-
-source .bashrc 
